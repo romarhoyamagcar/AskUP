5d5771b11d7d3d78407ddead9fd20fe6bc3c4aba
Fredrex Salac <fredrexsalac@gmail.com>
2025-11-21 14:50:30 +0800

    Update settings to use environment variables

--- askup/settings.py
+++ askup/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*hzm*m!$^w^m0(w*95sd&&e9$e*(0%hdb$%opvv---hy7#k8q1'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-*hzm*m!$^w^m0(w*95sd&&e9$e*(0%hdb$%opvv---hy7#k8q1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
